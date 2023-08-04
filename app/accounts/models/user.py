7a550a99eb7e9019b1b003b94e6d4a8de01d6d2d
Carlos Rodrigues <carloshcr5@gmail.com>
2023-08-04 15:20:36 -0300

    feat: Initial commit

---
+++ app/accounts/models/user.py
@@ -0,0 +1,14 @@
+###
+# Libs
+###
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext as _
+
+
+###
+# Model
+###
+
+
+class User(AbstractUser):
+    pass

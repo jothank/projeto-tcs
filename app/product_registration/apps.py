dfa18263ef00ad1447ef40402ba7db20153dd4eb
CARLOS HENRIQUE CORREA RODRIGUES <carlos.rodrigues3@alunos.sc.senac.br>
2023-08-18 19:28:21 -0300

    fix: product registration import app

--- app/product_registration/apps.py
+++ app/product_registration/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class ProductRegistrationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'product_registration'
+
+    def ready(self):
+        import app.product_registration.signals

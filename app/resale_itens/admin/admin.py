1daecd5681e536aa0e5a9bfdc72867422920043f
ALUNOS\carlos.rodrigues3 <carlos.rodrigues3@alunos.sc.senac.br>
2023-08-21 20:12:10 -0300

    delete: remove resale itens

--- app/resale_itens/admin/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from app.company.models.company import Address
-
-
-class ResaleItensAdmin(admin.ModelAdmin):
-    ...
-
-
-admin.site.register(Address, ResaleItensAdmin)

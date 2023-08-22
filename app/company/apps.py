2c2d184cd126dbe944e422b35accbbf6f0c55bf3
Ana Flavia de Freitas Corrêa <anaflaviacorrea@hotmail.com>
2023-08-22 19:54:04 -0300

    refactor: apps.py

--- app/company/apps.py
+++ app/company/apps.py
@@ -1,7 +1,7 @@
 from django.apps import AppConfig
 
 
-class AddressConfig(AppConfig):
+class CompanyConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'app.company'
 

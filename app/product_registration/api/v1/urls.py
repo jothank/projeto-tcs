9d260f72b1456abedd2e71819a6b2f28a0956572
Ana Flavia de Freitas Corrêa <anaflaviacorrea@hotmail.com>
2023-08-21 21:05:27 -0300

    refactor: adds a features

--- app/product_registration/api/v1/urls.py
+++ app/product_registration/api/v1/urls.py
@@ -3,12 +3,12 @@
 ##
 from django.urls import path, include
 from rest_framework import routers
-from app.company.api.v1.views.company_views import CompanyViewSet
+from app.product_registration.api.v1.views.product_registration_views import ProductRegistrationViewSet
 
 
 # Routers
 router = routers.SimpleRouter()
-router.register(r'company', CompanyViewSet, basename='company')
+router.register(r'produtregistration', ProductRegistrationViewSet, basename='product registration')
 
 # URLs
 urlpatterns = [

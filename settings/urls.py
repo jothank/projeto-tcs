690ea62a9fd53e56c4b2efc8abf7d388a27dd2f4
Carlos Rodrigues <carloshcr5@gmail.com>
2023-08-23 20:07:12 -0300

    refactor: Add url feedstock

--- settings/urls.py
+++ settings/urls.py
@@ -6,10 +6,10 @@ from .views import CustomLoginView, CustomResetPasswordView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.accounts.urls')),
-    path('', CustomLoginView.as_view(), name='custom_login'), 
+    path('', CustomLoginView.as_view(), name='custom_login'),
     path('reset_password/', CustomResetPasswordView.as_view(), name='reset_password'),
     path('api/', include('app.accounts.urls')),
-    # path('', include('app.feedstock.urls')),
+    path('', include('app.feedstock.urls')),
     path('', include('app.fixed_expense.urls')),
     path('', include('app.resale_item.urls')),
     path('', include('app.product.urls')),

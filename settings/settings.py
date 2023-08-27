0c63ae09600ffbaa5a2220f96201100acdffa022
Carlos Rodrigues <carloshcr5@gmail.com>
2023-08-26 22:31:56 -0300

    refactor: email subject prefix

--- settings/settings.py
+++ settings/settings.py
@@ -174,6 +174,7 @@ EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
+ACCOUNT_EMAIL_SUBJECT_PREFIX = '[GastroCustos]'
 
 REST_AUTH = {
     "PASSWORD_RESET_SERIALIZER": "app.accounts.api.v1.serializers.accounts.default.CustomPasswordResetSerializer",

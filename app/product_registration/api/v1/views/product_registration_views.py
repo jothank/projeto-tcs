a5044ac0fc5c560983de5a28408324c67c5b4efa
Ana Flavia de Freitas Corrêa <anaflaviacorrea@hotmail.com>
2023-08-28 19:07:39 -0300

    refactor: Product Registration Views

--- app/product_registration/api/v1/views/product_registration_views.py
+++ app/product_registration/api/v1/views/product_registration_views.py
@@ -21,31 +21,6 @@ from app.product_registration.api.v1.serializers.product_registration.default_se
 class ProductRegistrationViewSet(viewsets.ModelViewSet):
     queryset = ProductRegistration.objects.all()
     serializer_class = ProductRegistrationSerializer
-    # def perform_create(self, serializer):
-       
-    #     product_id = 1  
-    #     product = Product.objects.get(id=product_id)
-    #     serializer.save(product=product, percentage_loss=Decimal('5') / 100)
+ 
     
     
-    
-
-
-
-# class ProductRegistrationView(APIView):
-#     def post(self, request, format=None):
-#         product_data = request.data.get('product') 
-#         registration_data = request.data.get('registration')  
-
-        
-#         product_serializer = ProductSerializer(data=product_data)
-#         if product_serializer.is_valid():
-#             product_serializer.save()
-
-        
-#         registration_data['product'] = product_serializer.data.get('id')
-#         registration_serializer = ProductRegistrationSerializer(data=registration_data)
-#         if registration_serializer.is_valid():
-#             registration_serializer.save()
-
-#         return Response(status=status.HTTP_201_CREATED)

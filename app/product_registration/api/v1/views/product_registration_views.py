513088aab32ef6249c248e51cafd1550f6ae04b2
ALUNOS\carlos.rodrigues3 <carlos.rodrigues3@alunos.sc.senac.br>
2023-08-28 19:22:30 -0300

    change urls in setting

--- app/product_registration/api/v1/views/product_registration_views.py
+++ app/product_registration/api/v1/views/product_registration_views.py
@@ -8,8 +8,6 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from decimal import Decimal
-from .models import Product, ProductRegistration
-# from .serializers import ProductSerializer, ProductRegistrationSerializer
 from rest_framework import viewsets
 from app.product_registration.models.product_registration import ProductRegistration
 from app.product_registration.api.v1.serializers.product_registration.default_serializer import ProductRegistrationSerializer
@@ -18,34 +16,7 @@ from app.product_registration.api.v1.serializers.product_registration.default_se
 # Viewsets
 ###
 
+
 class ProductRegistrationViewSet(viewsets.ModelViewSet):
     queryset = ProductRegistration.objects.all()
     serializer_class = ProductRegistrationSerializer
-    # def perform_create(self, serializer):
-       
-    #     product_id = 1  
-    #     product = Product.objects.get(id=product_id)
-    #     serializer.save(product=product, percentage_loss=Decimal('5') / 100)
-    
-    
-    
-
-
-
-# class ProductRegistrationView(APIView):
-#     def post(self, request, format=None):
-#         product_data = request.data.get('product') 
-#         registration_data = request.data.get('registration')  
-
-        
-#         product_serializer = ProductSerializer(data=product_data)
-#         if product_serializer.is_valid():
-#             product_serializer.save()
-
-        
-#         registration_data['product'] = product_serializer.data.get('id')
-#         registration_serializer = ProductRegistrationSerializer(data=registration_data)
-#         if registration_serializer.is_valid():
-#             registration_serializer.save()
-
-#         return Response(status=status.HTTP_201_CREATED)

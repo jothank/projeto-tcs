033db8ff2b502402c1d15e3213f6c9b955594a45
Ana Flavia de Freitas Corrêa <anaflaviacorrea@hotmail.com>
2023-08-28 19:25:33 -0300

    refactor: Product Registration Views

--- app/product/signals/__init__.py
+++ app/product/signals/__init__.py
@@ -1,13 +1,13 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from app.product.models import Product  
+from app.product.models.product import Product  
 from decimal import Decimal
 from product_registration.models import ProductRegistration  
 
 @receiver(post_save, sender=Product)
 def add_percentage_loss(sender, instance, created, **kwargs):
     if created:
-        quantity_value = instance.productregistration_set.first().quantity
+        # quantity_value = instance.productregistration_set.first().quantity
         
         ProductRegistration.objects.create(
             product=instance,
--- app/product_registration/api/v1/views/product_registration_views.py
+++ app/product_registration/api/v1/views/product_registration_views.py
@@ -8,8 +8,6 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from decimal import Decimal
-from .models import Product, ProductRegistration
-# from .serializers import ProductSerializer, ProductRegistrationSerializer
 from rest_framework import viewsets
 from app.product_registration.models.product_registration import ProductRegistration
 from app.product_registration.api.v1.serializers.product_registration.default_serializer import ProductRegistrationSerializer

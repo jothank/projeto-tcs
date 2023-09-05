7308ff859c0f86830d341640dc1162f2fb7395f1
JONATHAN CARLOS COSTA <jonathan.costa1@alunos.sc.senac.br>
2023-09-05 19:48:18 -0300

    added urls logout swagger api

--- settings/urls.py
+++ settings/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.contrib.auth.views import LogoutView
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -23,4 +24,5 @@ urlpatterns = [
     path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('accounts/logout/', LogoutView.as_view(), name='logout'),
 ]

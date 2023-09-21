ab1c3622e06489eaed7892c830a1a8daa39b52fa
DESKTOP-R6K4UQL\Carlos <carloshcr5@gmail.com>
2023-09-21 16:57:07 -0300

    feat (frontend): ForceLogout

--- frontend/src/services/resealeItem.service.ts
+++ frontend/src/services/resealeItem.service.ts
@@ -2,24 +2,54 @@ import { ResaleItemType } from "types/resaleItem.types";
 import { BASE_URL } from "../config";
 import axios from "axios";
 import { validationToken, refreshToken } from "./auth.service";
+import Swal from "sweetalert2";
 
+const forceLogout = () => {
+  console.log("Chamando forceLogout"); // Adicione este log para depuração
+  Swal.fire({
+    icon: "error",
+    title: "Erro",
+    text: "Sua sessão expirou, faça login novamente.",
+    showConfirmButton: true,
+  }).then((result) => {
+    if (result.isConfirmed) {
+      window.location.href = "/login";
+    }
+  });
+};
 const getAuthorizationHeader = async (): Promise<{ Authorization: string }> => {
   try {
     await validationToken();
+    const localStorageAccessToken = localStorage.getItem("accessToken");
+    const accessToken = localStorageAccessToken
+      ? JSON.parse(localStorageAccessToken)
+      : null;
+
+    return {
+      Authorization: `Bearer ${accessToken}`,
+    };
   } catch (error: any) {
     if (error.message === "401") {
-      await refreshToken();
-      await validationToken();
+      try {
+        await refreshToken();
+        await validationToken();
+        const localStorageAccessToken = localStorage.getItem("accessToken");
+        const accessToken = localStorageAccessToken
+          ? JSON.parse(localStorageAccessToken)
+          : null;
+
+        return {
+          Authorization: `Bearer ${accessToken}`,
+        };
+      } catch (refreshError) {
+        forceLogout();
+        throw refreshError;
+      }
+    } else {
+      forceLogout();
+      throw error;
     }
   }
-  const localStorageAccessToken = localStorage.getItem("accessToken");
-  const accessToken = localStorageAccessToken
-    ? JSON.parse(localStorageAccessToken)
-    : null;
-
-  return {
-    Authorization: `Bearer ${accessToken}`,
-  };
 };
 
 const handleRequestError = (error: any) => {

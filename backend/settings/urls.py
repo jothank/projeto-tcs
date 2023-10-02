b8095228153fb49e91eda4fffda5fda2b2a013c6
DESKTOP-R6K4UQL\Carlos <carloshcr5@gmail.com>
2023-10-02 16:09:35 -0300

    feat (backend): Fix email verify and product create serializer

--- backend/settings/urls.py
+++ backend/settings/urls.py
@@ -1,9 +1,19 @@
+"""
+Root URL Configuration
+"""
+###
+# Libraries
+###
 from django.contrib import admin
 from django.urls import path, include
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
 
+
+###
+# Swagger
+###
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -17,6 +27,11 @@ schema_view = get_schema_view(
     public=True,
     permission_classes=(permissions.AllowAny,),
 )
+
+
+###
+# URL Patterns
+###
 
 
 urlpatterns = [

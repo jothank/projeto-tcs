d53d57bf8afcecfedaadbf30956ce29a283043c5
Ana Flavia de Freitas Corrêa <anaflaviacorrea@hotmail.com>
2023-11-02 23:17:18 -0300

    Duplication of fixedexpense.service with different code

---
+++ frontend/src/services/fixedexpense.service copy.ts
@@ -0,0 +1,54 @@
+import { BASE_URL } from "../config";
+import axios from "axios";
+import { getAuthorizationHeader } from "utils/GetHeader";
+import { FixedExpenseType, ExpenseType } from "types/FixedExpenses.types";
+
+
+
+export const saveExpenses = async (expenses: ExpenseType[]) => {
+  try {
+   
+    const expensesData = {
+      expenses: expenses.map(expense => ({
+        nameExpense: expense.nameExpense,
+        price: expense.price,
+        description: expense.description,
+       
+      })),
+    };
+
+    const response = await axios.post(BASE_URL + "expense/", { expenses: expensesData }, {
+      headers: await getAuthorizationHeader(),
+    });
+
+    return response.data;
+  } catch (error) {
+    return error;
+  }
+};
+
+export const setfixedExpense = async (fixedExpense: FixedExpenseType) => {
+  try {
+    const response = await axios.post(BASE_URL + "fixed_expense/", fixedExpense, {
+      headers: await getAuthorizationHeader(),
+    });
+
+    
+    return response.data;
+  } catch (error) {
+    return error;
+  }
+};
+
+export const getFixedExpense = async () => {
+  try {
+    const response = await axios.get(BASE_URL + "fixed_expense/", {
+      headers: await getAuthorizationHeader(),
+    });
+
+    
+    return response.data.results;
+  } catch (error) {
+    throw error;
+  }
+};

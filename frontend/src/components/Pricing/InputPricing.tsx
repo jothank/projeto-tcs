77b520c382f091b2133aeff0811e869b21074937
Jonathan Costa <jothank@hotmail.com>
2023-11-27 21:12:24 -0300

    ajuste virgula e ponto pricing

--- frontend/src/components/Pricing/InputPricing.tsx
+++ frontend/src/components/Pricing/InputPricing.tsx
@@ -3,24 +3,32 @@ import { Field } from "formik";
 import { TextField } from "@mui/material";
 import { PricingInputProps } from "types/pricing.types";
 
+const formatValue = (value: string): string => {
+  return value.replace(/,/g, '.');
+};
+
 export const PricingtemInput: React.FC<PricingInputProps> = ({
-    name,
-    label,
-    type,
-    value,
-  }) => (
-    <Field name={name}>
-      {({ field, meta }: any) => (
-        <TextField
-          {...field}
-          label={label}
-          type={type}
-          fullWidth
-          margin="dense"
-          error={meta.touched && meta.error !== undefined}
-          helperText={meta.touched && meta.error ? meta.error : ""}
-          value={value}
-        />
-      )}
-    </Field>
-  );
+  name,
+  label,
+  type,
+  value,
+}) => (
+  <Field name={name}>
+    {({ field, meta, form }: any) => (
+      <TextField
+        {...field}
+        label={label}
+        type={type}
+        fullWidth
+        margin="dense"
+        error={meta.touched && meta.error !== undefined}
+        helperText={meta.touched && meta.error ? meta.error : ""}
+        value={value}
+        onChange={(e) => {
+          const formattedValue = formatValue(e.target.value);
+          form.setFieldValue(name, formattedValue);
+        }}
+      />
+    )}
+  </Field>
+);
